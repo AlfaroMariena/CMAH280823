7871b03a485cdec264bcfbc4570f4592448e74d5
Cesia Mariena Alfaro Hernandez <alfarohernandezcesiamariena@gmail.com>
2023-08-28 08:23:13 -0600

    Inicio del proyecto de java web practica del dia lunes 28 de agosto del 2023,primer commit (8:23 AM)

---
+++ src/java/config/Conexion.java
@@ -0,0 +1,29 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Cortez
+ */
+public class Conexion {
+
+    public Connection getConexion()  {
+        try {
+             Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/CMA", "root", "12345678910");
+            System.out.println("Conexion Exitosa");
+            return conexion;
+
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            return null;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }return null;
+    }
+}
